5a5b1c93396272dba20ad903e336aa712eec5b31
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2023-11-23 10:03:48 +0100

    Add research page

---
+++ src/app/pages/research/research.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+  selector: 'app-research',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './research.component.html',
+  styleUrl: './research.component.scss'
+})
+export class ResearchComponent {
+
+}
