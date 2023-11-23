efcede29166837364620e08e1c924bbd9e501184
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2023-11-23 10:03:48 +0100

    Add research page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {CareerComponent} from './pages/career/career.component';
+import {ResearchComponent} from './pages/research/research.component';
 
 export const routes: Routes = [
   {title: 'Homepage', path: '', component: HomeComponent},
   {title: 'Career', path: 'career', component: CareerComponent},
+  {title: 'Research', path: 'research', component: ResearchComponent},
 ];
--- src/app/layout/default/default.component.ts
+++ src/app/layout/default/default.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SidebarComponent} from '../../components/sidebar/sidebar.component';
 import {HeaderComponent} from '../../components/header/header.component';
+import {FooterComponent} from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-default',
   standalone: true,
-  imports: [CommonModule, SidebarComponent, HeaderComponent],
+  imports: [CommonModule, SidebarComponent, HeaderComponent, FooterComponent],
   templateUrl: './default.component.html',
   styleUrl: './default.component.scss',
 })
