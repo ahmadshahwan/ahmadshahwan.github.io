600a183f919b552d0bae8ab307bbad080af11462
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2023-12-02 20:00:30 +0100

    Add education and remove contacts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,17 +4,17 @@ import {CareerComponent} from './pages/career/career.component';
 import {TeachingComponent} from './pages/teaching/teaching.component';
 import {ResearchComponent} from './pages/research/research.component';
 import {EventsComponent} from './pages/events/events.component';
-import {ContactsComponent} from './pages/contacts/contacts.component';
 import {PhdDefenseComponent} from './pages/phd-defense/phd-defense.component';
 import {PhdPresentationComponent} from './pages/phd-presentation/phd-presentation.component';
+import {EducationComponent} from './pages/education/education.component';
 
 export const routes: Routes = [
   {title: 'Homepage', path: '', component: HomeComponent},
   {title: 'Career', path: 'career', component: CareerComponent},
+  {title: 'Education', path: 'education', component: EducationComponent},
   {title: 'Teaching', path: 'teaching', component: TeachingComponent},
   {title: 'Research', path: 'research', component: ResearchComponent},
   {title: 'Events', path: 'events', component: EventsComponent},
-  {title: 'Contacts', path: 'contacts', component: ContactsComponent},
   {title: 'PHD defense', path: 'phd-defense', component: PhdDefenseComponent},
   {title: 'PHD defense', path: 'phd-presentation', component: PhdPresentationComponent},
 ];
