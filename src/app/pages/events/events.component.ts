52679facc586c6665b14d93bf4ac3968cd050316
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2023-12-02 20:13:15 +0100

    Frame pages in scrollables

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {ScrollableComponent} from '../../components/scrollable/scrollable.component';
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ScrollableComponent],
   templateUrl: './events.component.html',
   styleUrl: './events.component.scss'
 })
--- src/app/pages/research/research.component.ts
+++ src/app/pages/research/research.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {ScrollableComponent} from '../../components/scrollable/scrollable.component';
 
 @Component({
   selector: 'app-research',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ScrollableComponent],
   templateUrl: './research.component.html',
   styleUrl: './research.component.scss'
 })
