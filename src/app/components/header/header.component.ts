504575f0bf8f6c152220a0fb571ddbe71f0123a5
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2023-12-18 16:58:17 +0100

    Track current page in menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterLink} from '@angular/router';
+import {MenuLinkComponent} from '../menu-link/menu-link.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, MenuLinkComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
