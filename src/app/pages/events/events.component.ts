5c4ee7e5ca389f47bf4b9df41ed9a84e8e92b11e
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2024-01-20 22:19:19 +0100

    Read events from CMS

--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -1,14 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ScrollableComponent} from '../../components/scrollable/scrollable.component';
+import {EventRepositoryService} from '../../services/events/event-repository.service';
+import {Observable, of} from 'rxjs';
+import {EventComponent} from '../../components/event/event.component';
+import Event from '../../model/event';
 
 @Component({
   selector: 'app-events',
   standalone: true,
-  imports: [CommonModule, ScrollableComponent],
+  imports: [CommonModule, ScrollableComponent, EventComponent],
   templateUrl: './events.component.html',
   styleUrl: './events.component.scss'
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit {
 
+  events: Observable<Event[]> = of([]);
+
+  constructor(
+    private repository: EventRepositoryService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.events = this.repository.fetchAll();
+  }
 }
