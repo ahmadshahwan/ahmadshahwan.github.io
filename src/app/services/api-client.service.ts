4a56542ee96485e6961b694d315500c7ec2d00ca
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2024-02-11 17:01:54 +0100

    Transfer cached POST API requests

--- src/app/services/api-client.service.ts
+++ src/app/services/api-client.service.ts
@@ -23,8 +23,20 @@ export class ApiClientService {
   }
 
   fetch<T, P extends QueryParams>(query: Query<T, P>, variables?: P): Observable<T> {
+    // Add query hash to URL. This is a workaround of this issue: https://github.com/angular/angular/issues/54377
+    const url = `${environment.apiUrl}?idem_id=${this.hash(query)}`;
     return this.http
-      .post<{data: {data: T}}>(environment.apiUrl, {query, variables: variables})
+      .post<{data: {data: T}}>(url, {query, variables: variables})
       .pipe(map(r => r.data.data));
+  }
+
+  private hash(value: string): string {
+    let hash = 0;
+    for (const char of value) {
+      hash = ((hash << 5) - hash + char.charCodeAt(0)) << 0;
+    }
+    // Force positive number hash. 2147483647 = equivalent of Integer.MAX_VALUE.
+    hash += 2147483647 + 1;
+    return hash.toString();
   }
 }
