561cd5a35fa4877591f89886a378f832338f7d2b
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2024-02-17 17:40:28 +0100

    Linting class repo specs

--- src/app/services/teaching/class-repository.service.spec.ts
+++ src/app/services/teaching/class-repository.service.spec.ts
@@ -1,6 +1,6 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ClassRepositoryService } from './class-repository.service';
+import {ClassRepositoryService} from './class-repository.service';
 
 describe('ClassRepositoryService', () => {
   let service: ClassRepositoryService;
