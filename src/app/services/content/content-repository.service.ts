d7f08ee73f05bf653a958f31da796a2e85b3d42b
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2025-03-22 21:57:07 +0100

    Drop unused overloads

--- src/app/services/content/content-repository.service.ts
+++ src/app/services/content/content-repository.service.ts
@@ -10,12 +10,9 @@ export class ContentRepositoryService {
 
   constructor(
     private readonly apiClient: ApiClientService,
-  ) {
-  }
+  ) {}
 
-  fetch(): Observable<Website>;
-  fetch<T extends keyof Website>(field: T): Observable<Website[T]>;
-  fetch<T extends keyof Website>(field?: T): Observable<Website[T] | Website> {
-    return field ? this.apiClient.fetch(field) : this.apiClient.fetch();
+  fetch(): Observable<Website> {
+    return this.apiClient.fetch();
   }
 }
