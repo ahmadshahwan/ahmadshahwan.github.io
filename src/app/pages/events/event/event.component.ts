2026c23bf80f853445b65a4d85dd638d8085d9a7
Ahmad Shahwan <ahmad.shahwan@gmail.com>
2026-02-17 10:29:24 +0000

    events

--- src/app/pages/events/event/event.component.ts
+++ src/app/pages/events/event/event.component.ts
@@ -2,11 +2,12 @@ import {Component, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {Event} from '../../../model';
 import {RouterLinkAdapterDirective} from '../../../shared/router-link-adapter/router-link-adapter.directive';
+import {EntryComponent} from 'app/shared/entry/entry.component';
 
 @Component({
   selector: 'app-event',
   standalone: true,
-  imports: [CommonModule, RouterLinkAdapterDirective],
+  imports: [CommonModule, RouterLinkAdapterDirective, EntryComponent],
   templateUrl: './event.component.html',
   styleUrl: './event.component.scss'
 })
